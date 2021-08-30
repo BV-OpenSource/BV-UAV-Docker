64042c325cdd152628742c24e77f4028d58698cd
André Filipe <andre.ferreira@beyond-vision.pt>
2021-08-30 17:13:49 +0100

    Limit docker usage for total of CPU cores minus 2.

--- runDockers.sh
+++ runDockers.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 export TODAY=$(date +"%d")
+export CPU_AVAILABLE=$(getconf _NPROCESSORS_ONLN)
+export CPU_CORE=$(( CPU_AVAILABLE < 4 ? 1 : CPU_AVAILABLE-2 ))
 
 docker-compose up --build
