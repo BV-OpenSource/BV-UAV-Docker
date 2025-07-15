0e580b350d161de2bce3ec5cc10d1eb869addcfc
André Filipe <andre.ferreira@beyond-vision.com>
2025-07-15 18:05:15 +0100

    Update to noetic ROS version.

--- ControlDocker/rosCatkinEntrypoint.sh
+++ ControlDocker/rosCatkinEntrypoint.sh
@@ -3,5 +3,5 @@
 # Source ROS distro environment and local catwin workspace
 source "$HOME_WS/.profile" && source "/opt/ros/$ROS_DISTRO/setup.bash" && source "$CATKIN_WS/devel/setup.bash"
 
-command="$@ argEndpoint:=aeriya argCamType:=imx477"
+command="$@ argEndpoint:=prod argCamType:=imx477"
 bash -c "$command"
